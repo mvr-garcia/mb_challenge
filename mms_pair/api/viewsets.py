b0c97575294a48ba80ba3d6d29259d51735f3635
Marcos Garcia <mvrgarcia05@gmail.com>
2021-04-11 19:34:26 -0300

    API last 365 days restriction added

--- mms_pair/api/viewsets.py
+++ mms_pair/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.exceptions import NotAcceptable
 from mms_pair.models import Coin
 from . import serializers
 import datetime
@@ -17,10 +18,18 @@ class CryptoListView(generics.ListAPIView):
         """This view should return a list of all the crypto in specific timestamp range."""
         pair = self.kwargs['crypto']
 
+        # Checks if the request start date is less than 365 days.
+        # First: Finds the 365 day timestamps.
+        past = datetime.date.today() - datetime.timedelta(days=365 + 1)
+        past = int(time.mktime(datetime.datetime.strptime(str(past), "%Y-%m-%d").timetuple()))
 
-        past = self.kwargs['past']
+        # Second: If past is less then request start date, request isn't allowed
+        if self.kwargs['past'] < past:
+            raise NotAcceptable("Request start date less than 365 days isn't allowed")
+        else:
+            past = self.kwargs['past']
 
-        # If now timestamps aren't passed, now will be equal yesterday's timestamps
+        # If 'now' timestamps aren't passed, 'now' will be equal yesterday's timestamps
         if 'now' not in self.kwargs.keys():
             now = datetime.date.today() - datetime.timedelta(days=1)
             now = int(time.mktime(datetime.datetime.strptime(str(now), "%Y-%m-%d").timetuple()))
